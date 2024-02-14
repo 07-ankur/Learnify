fea7aa0109851ac64d298dbd7629d40ebbf24f64
07-ankur <im4ankur@gmail.com>
2024-02-14 18:09:05 +0530

    Fixing dir error

--- src/utils/contents/BlogContent.js
+++ src/utils/contents/BlogContent.js
@@ -1,21 +1,21 @@
 // Blog
 import image1 from "../../assets/images/Blogs/AI_blog.jpg";
-import image2 from "../../assets/images/blogs/VR_blog.jpeg";
-import image3 from "../../assets/images/blogs/Automation_blog.jpeg";
-import image4 from "../../assets/images/blogs/Cybersec_blog.jpeg";
-import image5 from "../../assets/images/blogs/Online_blog.jpg";
-import image6 from "../../assets/images/blogs/Peer_blog.jpeg";
-import image7 from "../../assets/images/blogs/Procastination_blog.webp";
-import image8 from "../../assets/images/blogs/Note_blog.jpeg";
-import image9 from "../../assets/images/blogs/resume_blog.jpeg";
-import image10 from "../../assets/images/blogs/Navigating_blog.jpeg";
-import image11 from "../../assets/images/blogs/Leadership_blog.jpeg";
-import image12 from "../../assets/images/blogs/Tech_blog.jpeg";
-import image13 from "../../assets/images/blogs/Cyber_blog.jpeg";
-import image14 from "../../assets/images/blogs/Startup_blog.png";
-import image15 from "../../assets/images/blogs/Elearn_blog.jpeg";
-import image16 from "../../assets/images/blogs/Linkedin_blog.jpeg";
-import image17 from "../../assets/images/blogs/Time_blog.webp";
+import image2 from "../../assets/images/Blogs/VR_blog.jpeg";
+import image3 from "../../assets/images/Blogs/Automation_blog.jpeg";
+import image4 from "../../assets/images/Blogs/Cybersec_blog.jpeg";
+import image5 from "../../assets/images/Blogs/Online_blog.jpg";
+import image6 from "../../assets/images/Blogs/Peer_blog.jpeg";
+import image7 from "../../assets/images/Blogs/Procastination_blog.webp";
+import image8 from "../../assets/images/Blogs/Note_blog.jpeg";
+import image9 from "../../assets/images/Blogs/resume_blog.jpeg";
+import image10 from "../../assets/images/Blogs/Navigating_blog.jpeg";
+import image11 from "../../assets/images/Blogs/Leadership_blog.jpeg";
+import image12 from "../../assets/images/Blogs/Tech_blog.jpeg";
+import image13 from "../../assets/images/Blogs/Cyber_blog.jpeg";
+import image14 from "../../assets/images/Blogs/Startup_blog.png";
+import image15 from "../../assets/images/Blogs/Elearn_blog.jpeg";
+import image16 from "../../assets/images/Blogs/Linkedin_blog.jpeg";
+import image17 from "../../assets/images/Blogs/Time_blog.webp";
 
 export const Blogs = {
   ITEMS: [
