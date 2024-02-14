cb87bbc2816da741d9dcf29528b2e985f5df9e83
07-ankur <im4ankur@gmail.com>
2024-02-14 18:13:00 +0530

    Fixing deployment err

--- src/utils/contents/MainContent.js
+++ src/utils/contents/MainContent.js
@@ -13,122 +13,122 @@ import Logo from "../../assets/images/Learnify_logo.png";
 import Logo_drk from "../../assets/images/Learnify_drk.png";
 
 //Topics
-import ReactLogo from "../../assets/images/Topics/React_logo.png"
-import NodeLogo from "../../assets/images/Topics/Node_logo.png"
-import SQLLogo from "../../assets/images/Topics/Sql_logo.png"
-import PythonLogo from "../../assets/images/Topics/Python_logo.png"
-import CppLogo from "../../assets/images/Topics/C++_logo.png"
-import MongoLogo from "../../assets/images/Topics/Mongo_logo.png"
-import HtmlLogo from "../../assets/images/Topics/Html_logo.png"
-import CssLogo from "../../assets/images/Topics/Css_logo.png"
-import JsLogo from "../../assets/images/Topics/Js_logo.png"
-import ExpressLogo from "../../assets/images/Topics/Express_logo.png"
+import ReactLogo from "../../assets/images/Topics/React_logo.png";
+import NodeLogo from "../../assets/images/Topics/Node_logo.png";
+import SQLLogo from "../../assets/images/Topics/Sql_logo.png";
+import PythonLogo from "../../assets/images/Topics/Python_logo.png";
+import CppLogo from "../../assets/images/Topics/C++_logo.png";
+import MongoLogo from "../../assets/images/Topics/Mongo_logo.png";
+import HtmlLogo from "../../assets/images/Topics/Html_logo.png";
+import CssLogo from "../../assets/images/Topics/Css_logo.png";
+import JsLogo from "../../assets/images/Topics/Js_logo.png";
+import ExpressLogo from "../../assets/images/Topics/Express_logo.png";
 
 export const footerContent = {
-    protocols: {
-      title: "Top Courses",
-      links: [
-        { title: "Python Learning" },
-        { title: "Web Development" },
-        { title: "Buisness Management" },
-        { title: "Graphic Design" },
-      ],
-    },
-    support: {
-      title: "Support",
-      links: [
-        { title: "Help center" },
-        { title: "Press room" },
-        { title: "Bug report" },
-        { title: "Contacts" },
-      ],
-    },
-    developers: {
-      title: "Developers",
-      links: [
-        { title: "Documentation" },
-        { title: "GitHub" },
-        { title: "Audit" },
-        { title: "Bug bounty" },
-      ],
-    },
-    subscribe: {
-      title: "Start Learning with us",
-      subtitle: "Get the latest and trusted content",
-    },
-    socials: [
-      // { icon: BookIcon },
-      { icon: RedditIcon },
-      { icon: TwitterIcon },
-      // { icon: ChatIcon },
-      { icon: TelegramIcon },
-      { icon: GitHubIcon },
+  protocols: {
+    title: "Top Courses",
+    links: [
+      { title: "Python Learning" },
+      { title: "Web Development" },
+      { title: "Buisness Management" },
+      { title: "Graphic Design" },
     ],
-    copyright: {
-      left: "© 2023 Learnify, All Rights Reserved.",
-      right: "Made with ❤️ by 07-Ankur",
-    },
-  };
-  
-  export const navbarContent = {
-    Logo,
-  };
-  
-  export const signupContent = {
-    Logo_drk,
-  };
+  },
+  support: {
+    title: "Support",
+    links: [
+      { title: "Help center" },
+      { title: "Press room" },
+      { title: "Bug report" },
+      { title: "Contacts" },
+    ],
+  },
+  developers: {
+    title: "Developers",
+    links: [
+      { title: "Documentation" },
+      { title: "GitHub" },
+      { title: "Audit" },
+      { title: "Bug bounty" },
+    ],
+  },
+  subscribe: {
+    title: "Start Learning with us",
+    subtitle: "Get the latest and trusted content",
+  },
+  socials: [
+    // { icon: BookIcon },
+    { icon: RedditIcon },
+    { icon: TwitterIcon },
+    // { icon: ChatIcon },
+    { icon: TelegramIcon },
+    { icon: GitHubIcon },
+  ],
+  copyright: {
+    left: "© 2023 Learnify, All Rights Reserved.",
+    right: "Made with ❤️ by 07-Ankur",
+  },
+};
 
-  export const Topics = {
-    SKILLS: [
-      {
-        key: 1,
-        title: "HTML",
-        logo:HtmlLogo
-      },
-      {
-        key: 2,
-        title: "CSS",
-        logo:CssLogo
-      },
-      {
-        key: 3,
-        title: "React JS",
-        logo:ReactLogo
-      },
-      {
-        key: 4,
-        title: "Node JS",
-        logo:NodeLogo
-      },
-      {
-        key: 5,
-        title: "Mongo DB",
-        logo:MongoLogo
-      },
-      {
-        key: 6,
-        title: "Express JS",
-        logo:ExpressLogo
-      },
-      {
-        key: 7,
-        title: "SQL",
-        logo:SQLLogo
-      },
-      {
-        key: 8,
-        title: "Python",
-        logo:PythonLogo
-      },
-      {
-        key: 9,
-        title: "C++",
-        logo:CppLogo
-      },
-      {
-        key: 10,
-        title: "JavaScript",
-        logo:JsLogo
-      },
-    ],
-  };
+export const navbarContent = {
+  Logo,
+};
+
+export const signupContent = {
+  Logo_drk,
+};
+
+export const Topics = {
+  SKILLS: [
+    {
+      key: 1,
+      title: "HTML",
+      logo: HtmlLogo,
+    },
+    {
+      key: 2,
+      title: "CSS",
+      logo: CssLogo,
+    },
+    {
+      key: 3,
+      title: "React JS",
+      logo: ReactLogo,
+    },
+    {
+      key: 4,
+      title: "Node JS",
+      logo: NodeLogo,
+    },
+    {
+      key: 5,
+      title: "Mongo DB",
+      logo: MongoLogo,
+    },
+    {
+      key: 6,
+      title: "Express JS",
+      logo: ExpressLogo,
+    },
+    {
+      key: 7,
+      title: "SQL",
+      logo: SQLLogo,
+    },
+    {
+      key: 8,
+      title: "Python",
+      logo: PythonLogo,
+    },
+    {
+      key: 9,
+      title: "C++",
+      logo: CppLogo,
+    },
+    {
+      key: 10,
+      title: "JavaScript",
+      logo: JsLogo,
+    },
+  ],
+};
