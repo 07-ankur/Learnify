f56168b07a398cfc9f43701d5627bfef20f85f40
07-ankur <im4ankur@gmail.com>
2024-04-13 22:30:51 +0530

    Same changes

--- client/src/container/Blog/Section2.jsx
+++ client/src/container/Blog/Section2.jsx
@@ -26,7 +26,7 @@ const Section2 = () => {
   const fetchData = async () => {
     try {
       const response = await axios.get(
-        "https://learnify-server-in.vercel.app/api/blog/getblogs"
+        "https://learnify-ev51.onrender.com/api/blog/getblogs"
       );
       setFilteredItems(response.data);
     } catch (error) {
@@ -37,7 +37,7 @@ const Section2 = () => {
   const filterAndSortItems = async (filterCategory, sortBy) => {
     try {
       const response = await axios.get(
-        "https://learnify-server-in.vercel.app/api/blog/getfilteredsortedblogs",
+        "https://learnify-ev51.onrender.com/api/blog/getfilteredsortedblogs",
         {
           params: {
             filterCategory,
